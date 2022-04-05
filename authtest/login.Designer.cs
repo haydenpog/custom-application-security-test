2c848a2940b050fe2c2a6249c8174f65f611f772
Hayden <52052725+haydenpog@users.noreply.github.com>
2022-04-05 14:06:32 -0400

    my testie

---
+++ authtest/login.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace authtest
+{
+    partial class login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.UseSystemPasswordChar = true;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(276, 119);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "login";
+            this.Text = "Login.cs";
+            this.Load += new System.EventHandler(this.login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ authtest/login.cs
@@ -0,0 +1,110 @@
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace authtest
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+        // List of exe names to close when in auth page.
+        string[] pog = {
+"Fiddler Everywhere",
+"ollydbg",
+"tcpview",
+"autoruns",
+"autorunsc",
+"filemon",
+"procmon",
+"idag",
+"hookshark",
+"peid",
+"lordpe",
+"regmon",
+"idaq",
+"idaq64",
+"ImmunityDebugger",
+"Wireshark",
+"dumpcap",
+"HookExplorer",
+"ImportREC",
+"PETools",
+"LordPE",
+"SysInspector",
+"proc_analyzer",
+"sysAnalyzer",
+"sniff_hit",
+"joeboxcontrol",
+"joeboxserver",
+"ida",
+"HTTPDebuggerSvc",
+"ResourceHacker",
+"ProcessHacker",
+// if i forget to remove the notepad please remove from ur version
+"notepad",
+"cheatengine-x86_64-SSE4-AVX2",
+"cheatengine-i386",
+"cheatengine-x86_64" };
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public static string Encode(string text)
+        {
+            var Bytes = System.Text.Encoding.UTF8.GetBytes(text);
+            return System.Convert.ToBase64String(Bytes);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+        static bool settingsReturn, refreshReturn;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 100% janky ignore my shiity cshart
+            for (int i = 0; i < 35; i++)
+            {
+
+                foreach (Process p in Process.GetProcessesByName(pog[i]))
+                {
+                    p.CloseMainWindow();
+                }
+            }
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+            // og hwid systems wouldn't recomend using this
+            string HWID;
+            HWID = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
+            HWID = Encode(HWID);
+            MessageBox.Show(HWID);
+            // I also recommend that you uh dont do this. Make a way so someone cant just run an autoresponder on it. Making it just copy is an easy way. Possibly change the hwid serversided so somone cant just make a pastebin
+            Clipboard.SetText(HWID);
+            // This disables any type of proxys used for programs like fiddler and maybe some normal selfcoded autoresponders using that method.
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyEnable", 0);
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+    }
+}
